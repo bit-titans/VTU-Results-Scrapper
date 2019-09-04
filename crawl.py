a6349bac889eccfbd0a7ed29e3f017b89f244bee
abhishekbn87 <abhishekbn87@gmail.com>
2019-09-04 12:02:08 +0530

    6th sem results july 2019

--- crawl.py
+++ crawl.py
@@ -4,12 +4,51 @@ import ocr
 import Write
 import WriteDB
 import xlrd
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 i = 1
 gpa = 0
 name = []
 names = []
 result = []
 lsubs = []
+ia1 = []
+ia2 = []
+ia3 = []
+ia4 = []
+ia5 = []
+ia6 = []
+ia7 = []
+ia8 = []
+ia9 = []
+ea1 = []
+ea2 = []
+ea3 = []
+ea4 = []
+ea5 = []
+ea6 = []
+ea7 = []
+ea8 = []
+ea9 = []
+t1 = []
+t2 = []
+t3 = []
+t4 = []
+t5 = []
+t6 = []
+t7 = []
+t8 = []
+t9 = []
+r1 = []
+r2 = []
+r3 = []
+r4 = []
+r5 = []
+r6 = []
+r7 = []
+r8 = []
+r9 = []
+usn_list = []
 loc = ("USN.xlsx")
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
@@ -21,20 +60,20 @@ while True:
         break
     gpa = 0
     s = requests.Session()
-    headers = {'Referer': 'http://results.vtu.ac.in/vitaviresultcbcs2018/index.php',
+    headers = {'Referer': 'https://results.vtu.ac.in/vitavicbcsjj19/index.php',
                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
                    'Upgrade-Insecure-Requests': '1',  'Cookie': 'PHPSESSID=u47uot7eg9j6eglqm951e3nfr7'
             , 'Connection': 'keep-alive'}
-    image = s.get("http://results.vtu.ac.in/resultsvitavicbcs_19/captcha_new.php", headers=headers)
+    image = s.get("https://results.vtu.ac.in/vitavicbcsjj19/captcha_new.php", headers=headers,verify=False)
     with open("snap.png", 'wb') as file:
         file.write(image.content)
     cap = ocr.get_ocr("snap.png")
     #USN = "1BI17CS"+str(format(i, '03d'))
-    url = "http://results.vtu.ac.in/resultsvitavicbcs_19/resultpage.php"
+    url = "https://results.vtu.ac.in/vitavicbcsjj19/resultpage.php"
     payload = {'lns': USN, 'captchacode': str(cap),
-                   'token': 'bEpYczFha3YrYUZMbjhZSDRydkgvZ1kyS3puN09PL09YcWd4bm1lUjBaa0p1eUZRdWFtbzNZamNVeGtnbGtTeHdBNWFydTVyUXJQU0lZYmUxQkRCTHc9PTo6Ydj2a8akPwQGJ9ITlWo7pw',
-                   'current_url': 'http://results.vtu.ac.in/resultsvitavicbcs_19/index.php'}
-    page = s.post(url, data=payload, headers=headers)
+                   'token': 'Yy96TVlDYTNlSWg2bFRjVGR5M3JONU0vaGlrQnlkYXl0TFBUaW1MeFNvMStxUXJhL2NkTEpKTUpmL0x5Z0pEVGRJbmYvMERVOWFZSzRqeFYwUHFQN3c9PTo6rom9zUebK3KcdZP03xVD+w==',
+                   'current_url': 'https://results.vtu.ac.in/vitavicbcsjj19/index.php'}
+    page = s.post(url, data=payload, headers=headers,verify=False)
     tree = html.fromstring(page.content)
     print("Sent USN:-"+USN)
    # print("Sent USN:-1BI17CS"+str(format(i, '03d')))
@@ -49,69 +88,71 @@ while True:
         exit(2)
     if "University Seat Number is not available or Invalid..!" in page.text:
         print("University Seat Number is not available or Invalid..!")
-        exit(-1)
+        # exit(-1)
     temp = page.text.find("Student Name")
     name.clear()
-    while (page.text[temp + 61] != "<"):
-        name.insert(len(name), page.text[temp + 61])
+    while (page.text[temp + 82] != "<"):
+        name.insert(len(name), page.text[temp + 82])
         temp += 1
     names.insert(len(names), ''.join(name))
-    if("Semester : 5" in page.text):
+    print(names)
+
+    if("Semester : 6" in page.text):
         iresult = {
             "USN": "-",
             "Name": "-",
-            "15CS51": ["-", "-", "-", "-"],
-            "15CS52": ["-", "-", "-", "-"],
-            "15CS53": ["-", "-", "-", "-"],
-            "15CS54": ["-", "-", "-", "-"],
-            "15CSL57": ["-", "-", "-", "-"],
-            "15CSL58": ["-", "-", "-", "-"],
-            "15ME562": ["-", "-", "-", "-"],
-            "15CS564": ["-", "-", "-", "-"],
-            "15CS562": ["-", "-", "-", "-"],
-            "15PHY561": ["-", "-", "-", "-"],
-            "15CS553": ["-", "-", "-", "-"],
+            "15CS61": ["-", "-", "-", "-"],
+            "15CS62": ["-", "-", "-", "-"],
+            "15CS63": ["-", "-", "-", "-"],
+            "15CS64": ["-", "-", "-", "-"],
+            "15CSL67": ["-", "-", "-", "-"],
+            "15CSL68": ["-", "-", "-", "-"],
+            "15CS651": ["-", "-", "-", "-"],
+            "15CS653": ["-", "-", "-", "-"],
+            "15CS664": ["-", "-", "-", "-"],
+            "15IM663": ["-", "-", "-", "-"],
+            "15MAT661": ["-", "-", "-", "-"],
         }
-        imarks1 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[3]')[0].text
-        imarks2 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[3]/div[3]')[0].text
-        imarks3 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div[3]')[0].text
-        imarks4 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div[3]')[0].text
-        imarks5 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div[3]')[0].text
-        imarks6 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[7]/div[3]')[0].text
-        imarks7 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[8]/div[3]')[0].text
-        imarks8 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[9]/div[3]')[0].text
-        emarks1 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[4]')[0].text
-        emarks2 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[3]/div[4]')[0].text
-        emarks3 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div[4]')[0].text
-        emarks4 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div[4]')[0].text
-        emarks5 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div[4]')[0].text
-        emarks6 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[7]/div[4]')[0].text
-        emarks7 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[8]/div[4]')[0].text
-        emarks8 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[9]/div[4]')[0].text
-        tmarks1 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[5]')[0].text
-        tmarks2 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[3]/div[5]')[0].text
-        tmarks3 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div[5]')[0].text
-        tmarks4 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div[5]')[0].text
-        tmarks5 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div[5]')[0].text
-        tmarks6 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[7]/div[5]')[0].text
-        tmarks7 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[8]/div[5]')[0].text
-        tmarks8 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[9]/div[5]')[0].text
-        result1 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[6]')[0].text
-        result2 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[3]/div[6]')[0].text
-        result3 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div[6]')[0].text
-        result4 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div[6]')[0].text
-        result5 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div[6]')[0].text
-        result6 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[7]/div[6]')[0].text
-        result7 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[8]/div[6]')[0].text
-        result8 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[9]/div[6]')[0].text
-        sub1 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]')[0].text
-        sub2 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[3]/div[1]')[0].text
-        sub3 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[4]/div[1]')[0].text
-        sub4 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[5]/div[1]')[0].text
-        sub5 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[6]/div[1]')[0].text
-        sub6 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[7]/div[1]')[0].text
-        sub7 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[8]/div[1]')[0].text
-        sub8 = tree.xpath('/html/body/div[2]/form/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/div[9]/div[1]')[0].text
+        imarks1 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[3]')[0].text
+        imarks2 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[3]')[0].text
+        imarks3 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div[3]')[0].text
+        imarks4 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[5]/div[3]')[0].text
+        imarks5 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[6]/div[3]')[0].text
+        imarks6 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div[3]')[0].text
+        imarks7 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div[3]')[0].text
+        imarks8 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[9]/div[3]')[0].text
+        emarks1 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[4]')[0].text
+        emarks2 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[4]')[0].text
+        emarks3 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div[4]')[0].text
+        emarks4 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[5]/div[4]')[0].text
+        emarks5 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[6]/div[4]')[0].text
+        emarks6 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div[4]')[0].text
+        emarks7 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div[4]')[0].text
+        emarks8 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[9]/div[4]')[0].text
+        tmarks1 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[5]')[0].text
+        tmarks2 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[5]')[0].text
+        tmarks3 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div[5]')[0].text
+        tmarks4 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[5]/div[5]')[0].text
+        tmarks5 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[6]/div[5]')[0].text
+        tmarks6 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div[5]')[0].text
+        tmarks7 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div[5]')[0].text
+        tmarks8 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[9]/div[5]')[0].text
+        result1 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[6]')[0].text
+        result2 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[6]')[0].text
+        result3 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div[6]')[0].text
+        result4 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[5]/div[6]')[0].text
+        result5 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[6]/div[6]')[0].text
+        result6 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div[6]')[0].text
+        result7 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div[6]')[0].text
+        result8 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[9]/div[6]')[0].text
+        sub1 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div[1]')[0].text
+        sub2 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[3]/div[1]')[0].text
+        sub3 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[4]/div[1]')[0].text
+        sub4 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[5]/div[1]')[0].text
+        sub5 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[6]/div[1]')[0].text
+        sub6 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div[1]')[0].text
+        sub7 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div[1]')[0].text
+        sub8 = tree.xpath('//*[@id="dataPrint"]/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/div/div[9]/div[1]')[0].text
         iresult["USN"] = USN
         iresult["Name"] = ''.join(name)
         iresult[sub1] = [imarks1, emarks1, tmarks1, result1]
--- ocr.py
+++ ocr.py
@@ -1,15 +1,32 @@
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageColor
+
 """
-                                            Created by ABHISHEK KOUSHIK B N 
+                                            Created by ABHISHEK KOUSHIK B N
                                                         AND
                                                        AKASH R
                                             on 01/02/2019
  """
 
 
+
 def get_ocr(src):
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe" # Specify path to tesseract.exe from Tessaract installation
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" # Specify path to tesseract.exe from Tessaract installation
     image = Image.open(src)
+    # image.show()
+    (left, upper, right, lower) = (52,9,150,29)
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(2.5)
+    enhancer = ImageEnhance.Sharpness(image)
+    image = enhancer.enhance(2.3)
+    enhancer = ImageEnhance.Color(image)
+    image = enhancer.enhance(0)
+    # image = ImageColor.getcolor()
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(10.5)
+    # image = image.crop((left,upper,right,lower))
+    # image.show()
     text = pytesseract.image_to_string(image)
+    # print("Hello")
+    # print(text)
     return text
